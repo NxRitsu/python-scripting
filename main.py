f88585be12bf0ab0f58165b480e2229f10c7fac9
NxRitsu <melvinlh76@gmail.com>
2023-11-13 17:25:53 +0100

    try to send mail with smtplib

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import smtplib
 import re
 from email.mime.text import MIMEText
 from http import HTTPStatus
+from dotenv import load_dotenv
+import os
 
 #Exercice 1
 with open("websites.txt", 'r', encoding='utf-8') as f:
@@ -15,15 +17,22 @@ with open("websites.txt", 'r', encoding='utf-8') as f:
             print("\n")
             print(r.status_code)
 #Exercice 2
-            if (r.status_code != HTTPStatus.OK):
+            if (r.status_code == HTTPStatus.OK):
+                load_dotenv()
+                EMAIL_SMTP = os.getenv("EMAIL_SMTP")
+                EMAIL_PASS = os.getenv("EMAIL_PASS")
+                EMAIL_USER = os.getenv("EMAIL_USER")
+                EMAIL_PORT = os.getenv("EMAIL_PORT")
+                DESTINATION_EMAIL = os.getenv("DESTINATION_EMAIL")
+
                 # Création de l'email
                 msg = MIMEText("Hello, this is a test email.")
                 msg['Subject'] = "Test Email"
-                msg['From'] = "melvinlh76@example.com"
-                msg['To'] = "receiver@example.com"
+                msg['From'] = EMAIL_USER
+                msg['To'] = DESTINATION_EMAIL
 
                 # Envoi de l'email
-                server = smtplib.SMTP('smtp.example.com')
-                server.login("sender@example.com", "password")
-                server.sendmail("sender@example.com", "receiver@example.com", msg.as_string())
+                server = smtplib.SMTP(EMAIL_SMTP)
+                server.login(EMAIL_USER, EMAIL_PASS)
+                server.sendmail(EMAIL_USER, DESTINATION_EMAIL, msg.as_string())
                 server.quit()
