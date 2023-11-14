0207e10c581c02c98af199de902e2f6eee79f018
NxRitsu <melvinlh76@gmail.com>
2023-11-14 19:06:42 +0100

    add send_mail() functionand complete exercice 2

--- main.py
+++ main.py
@@ -8,7 +8,30 @@ from dotenv import load_dotenv
 import os
 import socket
 
-#Exercice 1
+def send_mail(line):
+    load_dotenv()
+
+    try:
+        server = smtplib.SMTP_SSL(os.getenv('EMAIL_SMTP'), os.getenv('EMAIL_PORT'))
+        server.login(os.getenv('EMAIL_USER'), os.getenv('EMAIL_PASS'))
+
+        message = "Subject: Erreur\n\nImpossible de joindre le site : "+line
+
+        server.sendmail(
+            os.getenv('EMAIL_USER'),
+            os.getenv('DESTINATION_EMAIL'),
+            message
+        )
+
+        print("Email sent successfully!")
+
+    except Exception as e:
+        print(f"Error: {e}")
+
+    finally:
+        server.quit()
+
+#Exercice 1 & 2
 print("EXERCICE 1 :\n")
 with open("websites.txt", 'r', encoding='utf-8') as f:
     for line in f:
@@ -19,52 +42,11 @@ with open("websites.txt", 'r', encoding='utf-8') as f:
             print("Réponse ---> ", r.status_code, "\n")
         except requests.exceptions.MissingSchema:
             print(f"Erroneous Schema for url {line}")
+            send_mail(line)
         except requests.exceptions.SSLError:
             print(f"SSL missing for {line}")
-#Exercice 2
-"""with open("websites.txt", 'r', encoding='utf-8') as f:
-    for line in f:
-        if re.match(r'^https', line):
-            if (r.status_code == HTTPStatus.OK):
-                sender_email = "votre_email@gmail.com"
-                receiver_email = "destination@example.com"
-                subject = "Site down"
-                body = "Corps de l'e-mail"
-                #attachment_path = "/chemin/vers/votre/fichier/attaché.txt"
+            send_mail(line)
 
-                # Paramètres du serveur SMTP de Gmail
-                smtp_server = "smtp.gmail.com"
-                smtp_port = 587
-                smtp_username = "votre_email@gmail.com"
-                smtp_password = "your_password"
-
-                # Création du message
-                message = MIMEMultipart()
-                message['From'] = sender_email
-                message['To'] = receiver_email
-                message['Subject'] = subject
-
-                # Ajout du corps de l'e-mail
-                message.attach(MIMEText(body, 'plain'))
-
-                # Ajout d'une pièce jointe
-                with open(attachment_path, "rb") as attachment:
-                    part = MIMEApplication(attachment.read(), Name=os.path.basename(attachment_path))
-                    part['Content-Disposition'] = f'attachment; filename="{os.path.basename(attachment_path)}"'
-                    message.attach(part)
-
-                # Connexion au serveur SMTP
-                with smtplib.SMTP(smtp_server, smtp_port) as server:
-                    # Démarrer le chiffrement TLS
-                    server.starttls()
-
-                    # Authentification auprès du serveur SMTP
-                    server.login(smtp_username, smtp_password)
-
-                    # Envoi de l'e-mail
-                    server.send_message(message)
-
-                print("L'e-mail a été envoyé avec succès.")"""
 #Exercice 3
 print("EXERCICE 3 : \n")
 with open('websites_and_ports.txt', 'r') as fichier:
